d1fcc9ccaa0a14ac899b7b238d77f26ebc821b04
Byakko Kondo <bbbyk105@yahoo.co.jp>
2025-11-27 21:06:23 +0900

    関数の使い方

--- main.go
+++ main.go
@@ -3,5 +3,28 @@ package main
 import "fmt"
 
 func main(){
-	fmt.Println("Hello Go")
+	var a int = 10
+	b :=20
+
+	fmt.Println("a:", a)
+	fmt.Println("b:", b)
+
+	var name string ="Kondo"
+	var score float64 = 98.5
+	var isOk bool = true
+
+	fmt.Println("name", name)
+	fmt.Println("score", score)
+	fmt.Println("isOk", isOk)
+
+	result := add(5,7)
+	fmt.Println("5 + 7 =", result)
+	
+	result2 := add(6,7)
+	fmt.Println("6 + 7 =", result2)
+
+}
+
+func add(x int, y int) int {
+	return x+ y
 }
